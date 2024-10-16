3f1c8be514445edc96a332986e405f2561094f6c
hansel <hansel.loshaless@cimbniaga.co.id>
2024-10-16 14:52:59 +0700

    add await in error middleware

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -4,9 +4,10 @@ import { CustomError } from '../utils/customError';
 import { ErrorDTO } from '../dtos/errorDTO';
 import loggerService from '../services/loggerService';
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+export async function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   const statusCode = err instanceof CustomError ? err.statusCode : HttpStatusCode.INTERNAL_SERVER_ERROR;
 
-  loggerService.log(err);
+  await loggerService.log(err);
   res.status(statusCode).json({ message: err.message } as ErrorDTO);
 }
+  
